5574e32c22a6df7db307edcb6ab3f246e3c64740
Manohar Mankala <manohar_mankala@anfcorp.com>
2022-10-29 02:04:53 -0400

    update the deployed server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-catstronaughts-odyssey.herokuapp.com/',
   cache: new InMemoryCache()
 });
 
